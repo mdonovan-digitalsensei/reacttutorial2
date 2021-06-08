7ca6324f5771d3c9be27087fff9f1ea23660cef6
mdonovan-digitalsensei <63415197+mdonovan-digitalsensei@users.noreply.github.com>
2021-06-08 17:31:29 +0100

    before starting whatever it is I'm doing today

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
-    </div>    
+      <switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop/hats" component={HatsPage} />
+      </switch>
+    </div>
   );
 }
 
