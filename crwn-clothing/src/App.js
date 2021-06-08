a82f485f897cd361e2a3218831be31dcd55c44d6
mdonovan-digitalsensei <63415197+mdonovan-digitalsensei@users.noreply.github.com>
2021-06-08 19:15:08 +0100

    Created the basic shop preview page

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,20 +1,15 @@
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
-      <switch>
+      <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop/hats" component={HatsPage} />
-      </switch>
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
